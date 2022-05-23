e243438b02bfcfcfce911d1aec3eb01ecd208cab
sitelisting <102482051+sitelisting@users.noreply.github.com>
2022-05-23 12:51:56 +0200

    Update listedjobs.php

--- listedjobs.php
+++ listedjobs.php
@@ -8,24 +8,7 @@
    <link rel="stylesheet" href="listedjobs.css">
 </head>
 <body>
-<body>
-      <div class="main">
-         <div class="navbar">
-            <img src="list.png" class="menu-icon" onclick="myMenu()">
-            <div class="icon">
-               <h1 class="logo">ListingSite</h1>
-            </div>
-            <div class="menu">
-               <ul id="menuList">
-                  <li><a href="user_homepage.php">HOME</a></li>
-                  <li><a href="about.php">ABOUT</a></li>
-                  <li><a href="contact.php">CONTACT</a></li>
-                  <li><a href="jobs.php">POST</a></li>
-                  <li><a href="profile.php">PROFILE</a></li>
-               
-               </ul>
-            </div>
-         </div>
+
 <?php
 
 include("connectdb.php");
@@ -43,9 +26,8 @@ if(mysqli_num_rows($result) > 0){
                <th>Category</th>
                <th>Company Name</th>
                <th>Position</th>
-               <th>Job Link</th>
                <th>Job Contact</th>
-               <th>Job Descriptions</th>
+               <th>More Info and Apply</th>
            </tr>
          </thead>
          <tbody>
@@ -53,10 +35,10 @@ if(mysqli_num_rows($result) > 0){
                <th><?php echo $row['Job_Category'] ?></th>
                <th><?php echo $row['Company_Name'] ?></th>
                <th><?php echo $row['Job_Position'] ?></th>
-               <th><a  style='color:white;' href="#"><?php echo $row['Job_Link'] ?></a></th>
                <th><?php echo $row['Company_Number'].'<br>' .$row['Company_Email']?></th>
-               <th><?php echo $row['Job_Description']?>
+               <th><a  style='color:white;' href="<?php echo $row['Job_Link'] ?>">Click Here</a></th>
             </tr>
+          
             
       </tbody>
       <tr>
